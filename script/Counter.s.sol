8aa7c327fe8917e22f418fd4e251c8be1d3a916a
Igor Rončević <57319163+igorroncevic@users.noreply.github.com>
2026-05-11 19:41:09 +0200

    chore: add minimal solhint config (#13)
    ## Description
    Add a small Solhint setup that uses the local Solhint dependency.
    
    ## Context
    The root Solhint config extends `solhint:recommended` and only adds the
    intentional rules. The script and test folders have small override
    configs for their own style.
    
    ## Out of Scope
    This PR does not add Slither, Justfile commands, CI, or hooks.
    
    ## Testing Instructions
    - Merge PR #11 first.
    - Run `npm install --prefix dev`.
    - Run `dev/node_modules/.bin/solhint --max-warnings 0 'src/**/*.sol'`.
    - Run `dev/node_modules/.bin/solhint --max-warnings 0
    'script/**/*.sol'`.
    - Run `dev/node_modules/.bin/solhint --max-warnings 0
    'test/**/*.t.sol'`.
    
    ---------
    
    Co-authored-by: Federico Giacon <58218759+fedgiac@users.noreply.github.com>

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -4,16 +4,17 @@ pragma solidity ^0.8;
 import {Counter} from "../src/Counter.sol";
 import {Script} from "forge-std/Script.sol";
 
+/// @title CounterScript
+/// @author CoW DAO Developers
+/// @notice Script to deploy the Counter contract
 contract CounterScript is Script {
+    /// @notice The deployed Counter contract
     Counter public counter;
 
-    function setUp() public {}
-
+    /// @notice Run the script
     function run() public {
         vm.startBroadcast();
-
         counter = new Counter();
-
         vm.stopBroadcast();
     }
 }
